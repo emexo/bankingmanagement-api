d7b3cc8718e8b109bb21527c05ac70ce3532c579
emexo <info@emexotechnologies.com>
2025-06-09 19:14:21 +0530

    initial commit

---
+++ src/main/java/com/bankingmanagement/BankingmanagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.bankingmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingmanagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingmanagementApiApplication.class, args);
+	}
+
+}
