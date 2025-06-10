e92fd39e0879d8352bba1fecee1e1e36a641519a
emexo <info@emexotechnologies.com>
2025-06-10 08:32:48 +0530

    Added logger configuration

--- src/main/java/com/bankingmanagement/BankingManagement.java
+++ src/main/java/com/bankingmanagement/BankingManagement.java
@@ -1,13 +1,16 @@
 package com.bankingmanagement;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Log4j2
 @SpringBootApplication
 public class BankingManagement {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankingManagement.class, args);
+		log.info("#### Application has been started ####");
 	}
 
 }
