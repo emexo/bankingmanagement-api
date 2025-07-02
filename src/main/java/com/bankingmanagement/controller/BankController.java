28996cc2e4ce9b46183201c9c9c5c102f43bb103
emexo <info@emexotechnologies.com>
2025-07-02 07:36:42 +0530

    Added GET Banks api

---
+++ src/main/java/com/bankingmanagement/controller/BankController.java
@@ -0,0 +1,46 @@
+package com.bankingmanagement.controller;
+
+import com.bankingmanagement.exception.BankDetailsNotFoundException;
+import com.bankingmanagement.model.BankTO;
+import com.bankingmanagement.service.BankService;
+import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Log4j2
+@RestController
+@RequestMapping("/api/v1/banks")
+public class BankController {
+
+    @Autowired
+    private BankService bankService;
+
+    // http://localhost:8080/api/v1/banks  GET
+    @GetMapping
+    public ResponseEntity<List<BankTO>> getAllBanks() {
+        log.info("Fetching all banks");
+      List<BankTO> bankTOS = null;
+
+      try{
+          bankTOS = bankService.getAllBanks();
+          log.info("Successfully fetched all banks");
+      }catch (BankDetailsNotFoundException ex){
+          log.error("Bank details not found: {}", ex.getMessage());
+          return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+      }catch (Exception e) {
+          log.error("Error fetching banks: {}", e.getMessage());
+          return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+        return new ResponseEntity<>( bankTOS, HttpStatus.OK);
+    }
+
+}
