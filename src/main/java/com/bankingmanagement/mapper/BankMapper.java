1292e8ba5206c3a706c7c3fe6ad484658b479862
emexo <info@emexotechnologies.com>
2025-10-25 11:21:31 +0530

    Added service implementation for get all the banks

--- src/main/java/com/bankingmanagement/mapper/BankMapper.java
+++ src/main/java/com/bankingmanagement/mapper/BankMapper.java
@@ -2,11 +2,14 @@ package com.bankingmanagement.mapper;
 
 import com.bankingmanagement.entity.Bank;
 import com.bankingmanagement.model.BankTO;
+import jakarta.persistence.Column;
 import jdk.dynalink.linker.LinkerServices;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Component
 public class BankMapper {
     public List<BankTO> convertToBankTo(List<Bank> bankList){
         List<BankTO> bankTOList = bankList.stream().map(bank -> {
